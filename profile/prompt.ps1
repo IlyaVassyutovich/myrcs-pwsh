c55f975eaf6693ac1c485596648a84652da95b21
Ilya Vassyutovich <vassyutovich@mindbox.ru>
2021-04-22 10:07:00 +0300

    Implement terminal title update for git-repo

--- profile/prompt.ps1
+++ profile/prompt.ps1
@@ -1,17 +1,34 @@
+function Update-WindowTitle {
+	$GitStatus = Get-GitStatus -Force
+	if ($GitStatus) {
+		$NewTitleCandidate = "git: $($GitStatus.RepoName)"
+	}
+	else {
+		$NewTitleCandidate = "pwsh"
+	}
+
+	if ($global:__LastSetTitle -ne $NewTitleCandidate) {
+		Write-Debug "Updating window title"
+		$Host.UI.RawUI.WindowTitle = $NewTitleCandidate
+		$global:__LastSetTitle = $NewTitleCandidate
+	}
+}
+
 function Prompt {
 	$ExitCodeBackup = $global:LASTEXITCODE
 	$TimeString = Get-Date -Format "HH:MM:ss"
 	$CurrentLocation = $executionContext.SessionState.Path.CurrentLocation
 	$RolePrompt = if (Test-IsAdmin) { "#" } else { "$" }
 
-
-	if (Get-GitStatus -Force) {
+	$GitStatus = Get-GitStatus -Force
+	if ($GitStatus) {
 		$VcsPrompt = "─$(Write-VcsStatus)"
 	}
 	else {
 		$VcsPrompt = ""
 	}
 
+	Update-WindowTitle
 
 	$global:LASTEXITCODE = $ExitCodeBackup
 
