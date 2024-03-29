1d622147e8a2915dbcb806a4cfa767fcd7b0b9ab
Ilya Vassyutovich <me@iv.link>
2024-03-29 18:16:55 +0300

    Drop mindbox module import

--- profile/profile.ps1
+++ profile/profile.ps1
@@ -6,22 +6,7 @@ $LastExitCodeBackup = $LASTEXITCODE
 . (Join-Path $PSScriptRoot helpers.ps1)
 . (Join-Path $PSScriptRoot prompt.ps1)
 . (Join-Path $PSScriptRoot k8s-helpers.ps1)
-
-$ModuleRepository = "agamemnon.ivh"
-
-Write-Debug "Setup Mindbox env | check env"
-$IsMindboxWorkStation = ${ENV:mindbox/isMindboxWorkStation}
-Write-Debug "Setup Mindbox env | $IsMindboxWorkStation"
-if ($IsMindboxWorkStation -eq $True) {
-	Initialize-ModuleCached `
-		-Name "myrcs-mindbox" `
-		-RequiredVersion "1.1.1" `
-		-Repository $ModuleRepository
-}
-Write-Debug "Setup Mindbox env | done"
-
 . (Join-Path $PSScriptRoot psreadline.ps1)
 . (Join-Path $PSScriptRoot posh-git.ps1)
-
 
 $global:LASTEXITCODE = $LastExitCodeBackup
